de78ddb37d458efd3dae75e2cb83b5382de01ca2
y0usaf <OA99@Outlook.com>
2026-08-05 13:17:22 -0400

    dev: add adb wireless-debugging QR pairing helper

--- modules/dev/android-tools.nix
+++ modules/dev/android-tools.nix
@@ -3,7 +3,12 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  adbWirelessQrPython = pkgs.python3.withPackages (ps: [
+    ps.qrcode
+    ps.zeroconf
+  ]);
+in {
   options.user.dev.android-tools = {
     enable = lib.mkEnableOption "android-tools (adb, fastboot)";
   };
@@ -11,6 +16,9 @@
   config = lib.mkIf config.user.dev.android-tools.enable {
     environment.systemPackages = [
       pkgs.android-tools
+      (pkgs.writeShellScriptBin "adb-wireless-qr" ''
+        exec ${adbWirelessQrPython}/bin/python3 ${./adb-wireless-qr.py} "$@"
+      '')
     ];
   };
 }
