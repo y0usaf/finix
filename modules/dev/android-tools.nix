26f507d45e3f777af68a86745be604b16a6bfcd4
y0usaf <OA99@Outlook.com>
2026-08-16 00:42:18 -0400

    dev: drop adb-wireless-qr pairing helper

--- modules/dev/android-tools.nix
+++ modules/dev/android-tools.nix
@@ -3,22 +3,13 @@
   lib,
   pkgs,
   ...
-}: let
-  adbWirelessQrPython = pkgs.python3.withPackages (ps: [
-    ps.qrcode
-    ps.zeroconf
-  ]);
-in {
+}: {
   options.user.dev.android-tools = {
     enable = lib.mkEnableOption "android-tools (adb, fastboot)";
   };
-
   config = lib.mkIf config.user.dev.android-tools.enable {
     environment.systemPackages = [
       pkgs.android-tools
-      (pkgs.writeShellScriptBin "adb-wireless-qr" ''
-        exec ${adbWirelessQrPython}/bin/python3 ${./adb-wireless-qr.py} "$@"
-      '')
     ];
   };
 }
