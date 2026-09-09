0ac26db698d02b3f575b8a36459e5225eb8ef45f
y0usaf <74448287+y0usaf@users.noreply.github.com>
2026-09-09 13:23:15 -0400

    Simplify gaming launchers and conditional package lists

--- modules/gaming/elden-ring/coop.nix
+++ modules/gaming/elden-ring/coop.nix
@@ -57,35 +57,37 @@ in {
 
       "${gameDir}/SeamlessCoop/ersc_settings.ini" = {
         generator = lib.generators.toINI {};
-        value = lib.recursiveUpdate {
-          "GAMEPLAY" = {
-            allow_invaders = 0;
-            death_debuffs = 1;
-            allow_summons = 1;
-            overhead_player_display = 0;
-            skip_splash_screens = 1;
-            append_steam_id_to_players = 0;
-            always_spectate_on_death = 0;
-            default_boot_master_volume = 5;
-          };
-          "SCALING" = {
-            enemy_health_scaling = 35;
-            enemy_damage_scaling = 0;
-            enemy_posture_scaling = 15;
-            boss_health_scaling = 100;
-            boss_damage_scaling = 0;
-            boss_posture_scaling = 20;
-          };
-          "PASSWORD" = {
-            cooppassword = cfg.coop.password;
-          };
-          "SAVE" = {
-            save_file_extension = "co2";
-          };
-          "LANGUAGE" = {
-            mod_language_override = "";
-          };
-        } cfg.coop.settings;
+        value =
+          lib.recursiveUpdate {
+            "GAMEPLAY" = {
+              allow_invaders = 0;
+              death_debuffs = 1;
+              allow_summons = 1;
+              overhead_player_display = 0;
+              skip_splash_screens = 1;
+              append_steam_id_to_players = 0;
+              always_spectate_on_death = 0;
+              default_boot_master_volume = 5;
+            };
+            "SCALING" = {
+              enemy_health_scaling = 35;
+              enemy_damage_scaling = 0;
+              enemy_posture_scaling = 15;
+              boss_health_scaling = 100;
+              boss_damage_scaling = 0;
+              boss_posture_scaling = 20;
+            };
+            "PASSWORD" = {
+              cooppassword = cfg.coop.password;
+            };
+            "SAVE" = {
+              save_file_extension = "co2";
+            };
+            "LANGUAGE" = {
+              mod_language_override = "";
+            };
+          }
+          cfg.coop.settings;
       };
     };
   };
